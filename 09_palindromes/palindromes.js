3a84ed692f6096de1cc12ca1c8d7b8286f3a76cc
stefhacker <stefhaecker@gmail.com>
2025-04-10 16:22:23 +0200

    completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string
+        .split('')
+        .filter(char => char !== '!' && char !== '.' && char !==' ' && char !==',');
+        
+
+    let copy = [];
+    for(i = 0; i < string.length; i++){
+        copy[i] = string[string.length - i - 1];
+        }
+    
+    string = string.join('');
+    copy = copy.join('')
+   
+    
+    if(copy == string){
+        return true
+
+    } else {
+        return false
+    }
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
