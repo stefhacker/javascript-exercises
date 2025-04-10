519bad1fcc1d4a98051410e276be1e6b0b93d82a
stefhacker <stefhaecker@gmail.com>
2025-04-10 16:36:27 +0200

    completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const n_cleansed = parseInt(n);
+    if( n_cleansed < 0){
+        return 'OOPS'
+    }
+    if( n_cleansed == 0){
+        return 0
+    }
+    if( n_cleansed == 1){
+        return 1
+    } else {
+        return fibonacci(n_cleansed - 1) + fibonacci(n_cleansed - 2) 
+    }
+   
 
 };
 
