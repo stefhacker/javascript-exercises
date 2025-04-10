8394bc664258e934d0b0bbe52f610f69c1cc788f
stefhacker <stefhaecker@gmail.com>
2025-04-10 15:48:43 +0200

    completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,30 @@
-const add = function() {
+const add = function(a, b) {
+   return a + b
+};
+
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const subtract = function() {
+const sum = function(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0)
 	
 };
 
-const sum = function() {
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1)
+
+};
+
+const power = function(a, b) {
+  return Math.pow(a, b)
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  if (n == 0){return 1}
+  return factorial(n-1) * n
 	
 };
 
