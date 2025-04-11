0dacb39bde27175c1ff6503a7c9a942181d8e152
stefhacker <stefhaecker@gmail.com>
2025-04-11 11:01:11 +0200

    completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let currentYear = new Date().getFullYear();
 
+    let ages = people.map((person) => {
+      if (person.yearOfDeath === undefined) {
+        return currentYear - person.yearOfBirth;
+      } else {
+        return person.yearOfDeath - person.yearOfBirth;
+      }
+    });
+    return (people[ages.indexOf(Math.max(...ages))])
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
